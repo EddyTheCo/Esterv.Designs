19e9ef1415e7975cede1791dbad02799e85ac696
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-08-06 13:19:42 +0200

    Fix plus clipboard (#25) (#26)
    * added the textclipboard object
    
    * used the textclipboard object that work for chrome
    
    * fixed the plus button animation

--- include/mydesigns.hpp
+++ include/mydesigns.hpp
@@ -1,9 +1,37 @@
 #pragma once
 #include<QString>
-
+#include<QObject>
+#include<QString>
+#include <QtQml/qqmlregistration.h>
+#include <QClipboard>
+#include <QGuiApplication>
 
 //foo namespace to force the linker to link the backing library composed only of qml files
 namespace fooDesign 
 {
 	QString fooPrint(void);
+};
+
+class TextClipboard :public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString  text MEMBER m_text NOTIFY textChanged)
+    QML_ELEMENT
+
+public:
+    TextClipboard(QObject *parent = nullptr):QObject(parent),m_text(QString()),clipboard(QGuiApplication::clipboard())
+    {
+
+    }
+    Q_INVOKABLE void copy()const
+    {
+        clipboard->setText(m_text);
+    }
+signals:
+    void textChanged();
+
+public:
+    QString m_text;
+    QClipboard *clipboard;
 };
