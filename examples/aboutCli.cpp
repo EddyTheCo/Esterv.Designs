9a95102c5cf5a873b45d0e0280bb1dba7b16a7cf
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-07-13 12:43:51 +0200

    added example to test the about class

---
+++ examples/aboutCli.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+#include "about.hpp"
+
+using namespace esterv::CustomControls;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    qDebug() << "name:" << AboutThis::instance()->name();
+    qDebug() << "version:" << AboutThis::instance()->version();
+    qDebug() << "buildArch:" << AboutThis::instance()->buildArch();
+    qDebug() << "buildTime:" << AboutThis::instance()->buildTime();
+
+    return app.exec();
+}
